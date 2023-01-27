2d2ff609d337fa8ec947b6f8bc4d1ea0bbd4254a
Christina <bihtya@mail.ru>
2023-01-27 20:48:54 +0300

    Решение задачи

---
+++ Exp03/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая на вход принимает число N и выдает таблицу кубов чисел от 1 до N.
+
+int ValueInput()
+{
+    Console.Write("Введите число: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+void cubeNumber(int number)
+{
+    
+    for (int i = 1; i <= number; i++)
+    {
+        double cube = Math.Pow (i, 3);
+        Console.WriteLine($"{i} = " + cube);
+    } 
+}
+int number = ValueInput();
+cubeNumber(number);
